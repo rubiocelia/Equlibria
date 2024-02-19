f27a4b58fd2b6478f940e98772aa00e6fcf96d91
DavidOS10 <onatesanchezdavid@gmail.com>
2024-02-19 13:16:46 +0100

    cambios registro

--- Proyecto_integrador/registrarse.php
+++ Proyecto_integrador/registrarse.php
@@ -1,62 +1,11 @@
-<?php
-
-require_once("conecta.php");
-
-$conexion = getConexion();
-
-// Iniciamos sesión
-session_start();
-
-// Verificamos si se ha enviado un formulario
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Obtenemos datos del formulario
-    $usuario_pacientes = $_POST['usuario_pacientes'];
-    $contrasena_pacientes = $_POST['contrasena_pacientes']; // Contraseña en texto plano
-    $nombre_pacientes = $_POST['nombre_pacientes'];
-    $apellidos_pacientes = $_POST['apellidos_pacientes'];
-    $telefono_paciente = $_POST['telefono_paciente'];
-    $mail_pacientes = $_POST['mail_pacientes'];
-
-    // Hashing de la contraseña
-    $contrasena_pacientes_hash = password_hash($contrasena_pacientes, PASSWORD_DEFAULT);
-
-    // Preparamos la consulta SQL para insertar el hash de la contraseña
-    $sql_insert_paciente = "INSERT INTO pacientes (usuario_pacientes, contrasena_pacientes, nombre_pacientes, apellidos_pacientes, telefono_paciente, mail_pacientes) 
-                            VALUES (?, ?, ?, ?, ?, ?)";
-
-    // Preparar la sentencia
-    if ($stmt = $conexion->prepare($sql_insert_paciente)) {
-        // Vinculamos los parámetros a la sentencia preparada
-        $stmt->bind_param("ssssis", $usuario_pacientes, $contrasena_pacientes_hash, $nombre_pacientes, $apellidos_pacientes, $telefono_paciente, $mail_pacientes);
-
-        // Ejecutamos la sentencia preparada
-        if ($stmt->execute()) {
-            header("Location: inicio_sesion.php");
-            exit();
-        } else {
-            echo "Error al crear el paciente: " . $stmt->error;
-        }
-
-        // Cerramos la sentencia preparada
-        $stmt->close();
-    } else {
-        echo "Error al preparar la consulta: " . $conexion->error;
-    }
-
-    // Cerramos conexión
-    $conexion->close();
-}
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
-    <script src="validacion_registrar.js"></script>
-    <title>Crear cuenta - Equilibria</title>
+    <link rel="stylesheet" href="../Proyecto_integrador/css/registro.css">
+    <script src="../Proyecto_integrador/js/validacion_registrar.js"></script>
+    <title>prueba</title>
 </head>
 <body class="registroFormulario">
     <div class="subrayadoFormularioRegistro">
@@ -97,18 +46,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     <input type="text" id="usuario_pacientes" name="usuario_pacientes">
                 </div>
                 <div class="form-column">
-                    <label for="contrasena_pacientes">Contraseña:</label>
-                    <input type="password" id="contrasena_pacientes" name="contrasena_pacientes">
-                </div>
-                <div class="containerPassword">
-                        <div class="strengthMeter"></div>
+                    <div class="inputArea">
+                        <label for="contrasena_pacientes">Contraseña:</label>
+                        <input type="password" id="contrasena_pacientes" name="contrasena_pacientes">
                     </div>
+                    <div class="container">
+                        <div class="strengthMeter"></div>
+                    </div>    
+                </div>
             </div>
             <hr>
             <p>¿Tienes una cuenta?<a href="inicio_sesion.php"> Inicia sesión en tu cuenta</a></p>
             <button type="submit">Crear cuenta</button>
         </form>
-</div>
+    </div>
+    <script src="../Proyecto_integrador/js/registro.js"></script>
 </body>
-
 </html>
